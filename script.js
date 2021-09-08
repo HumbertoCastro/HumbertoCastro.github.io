46ca92f490ccb5e2843cca0c9be20de728936723
humbertoCastro <dedebarbos@hotmail.com>
2021-09-07 21:26:15 -0300

    desing finalizado

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ window.addEventListener('scroll',blurheader)
 
 function blurheader() {
     let value = window.scrollY;
+    let back = document.querySelector(".backtotop");
     let header = document.querySelector("main");
     console.log(value*0.001);
     header.style.opacity = "" +  (1-value*0.001);
+    back.style.opacity = "" +  (0+value*0.01);
 }
