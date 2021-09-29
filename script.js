30b3387dda3620df3bf9c309ced7353574a856a1
humbertoCastro <dedebarbos@hotmail.com>
2021-09-29 14:09:13 -0300

    site atualizado

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function blurheader() {
     let value = window.scrollY;
     let back = document.querySelector(".backtotop");
     let header = document.querySelector("main");
-    console.log(value*0.001);
     header.style.opacity = "" +  (1-value*0.001);
     back.style.opacity = "" +  (0+value*0.01);
 }
