45f07345d4f1e1a59a2b7737fd5cf400c2b30be1
humbertoCastro <dedebarbos@hotmail.com>
2022-12-12 20:22:53 -0300

    portifilio limpesa

--- Colorguess/script.js
+++ Colorguess/script.js
@@ -1,50 +1,28 @@
-function generetedcolors(numerodecores){
-    for(i=0;i < numerodecores; i++){
-        let randomColor = "rgb("+(Math.round(Math.random()*260))+","+(Math.round(Math.random()*260))+","+(Math.round(Math.random()*260))+")";
-        let ball = document.createElement("li");
-        ball.className = "ball hover";
-        ball.style.backgroundColor = randomColor;
-        ball.addEventListener("click",guess);
-        document.querySelector(".lista").appendChild(ball);
-    }
-    rgbtext();
-}
-function rgbtext(){
-    let random = Math.round(Math.random()*5);
-    console.log(random);
-    let valor =document.querySelectorAll(".ball")[random].style.backgroundColor;
-    let Rgb = [];
-    for(i=3;i<valor.length;i++){
-        Rgb.push(valor[i]);
-    }
-    console.log("rgb - " + Rgb);
-    valor = "";
-    for(i=0;i<Rgb.length;i++){
-        valor += Rgb[i];
-    }
-    console.log("Valor " + valor);
-    document.querySelector("p").innerText = valor;
-}
-function guess(origin){
-    console.log(origin.target);
-    if(origin.target.style.backgroundColor == "rgb"+document.querySelector("p").innerText){
-        document.querySelector("#answer").innerText = "Acertou!";     
-        Reset(); 
-        addPoint();   
-    }
-    else{
-        document.querySelector("#answer").innerText = "Errou!Tente novamente!";      
-        Reset();   
-    }
-}
-function addPoint(){
-    document.querySelector("#score").innerText = parseInt(document.querySelector("#score").innerText) + 3;
-}
-function Reset(){
-    let lista = document.querySelectorAll(".ball");
-    for(i=0;i<lista.length;i++){
-        lista[i].parentElement.removeChild(lista[i]);
-    } 
-    generetedcolors(6);
-}
-generetedcolors(6);
+function setup() {
+    const cards = [...document.querySelectorAll(".card")]
+    cards.map((card) => addEventListener("click", (event) => {
+        cards.map((x) => x.innerHTML = "down");
+        event.target.innerHTML = "up";
+        console.log(event.target, cards);
+    }))
+  }
+  
+  // Example case. 
+  document.body.innerHTML = `
+  <table id="card-container">
+    <tbody>
+      <tr>
+        <td class="card">down</td>
+        <td class="card">up</td>
+      </tr>
+      <tr>
+        <td class="card">down</td>
+        <td class="card">down</td>
+      </tr>
+    </tbody>
+  </table>`;
+  
+  setup();
+  
+  document.getElementsByClassName("card")[0].click();
+  console.log(document.body.innerHTML);
