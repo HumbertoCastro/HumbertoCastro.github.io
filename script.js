ed399e7340bf4e6f4a672e7ee61fc0d3b55cfeb2
humbertoCastro <dedebarbos@hotmail.com>
2023-01-27 14:28:17 -0300

    footer

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll',blurheader)
-
-function blurheader() {
-    let value = window.scrollY;
-    let back = document.querySelector(".backtotop");
-    let header = document.querySelector("main");
-    header.style.opacity = "" +  (1-value*0.001);
-    back.style.opacity = "" +  (0+value*0.01);
-}
